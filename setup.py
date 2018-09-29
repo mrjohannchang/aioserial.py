7bcfb63eaabd7eb743ab88df3c6ebdf74de63342
Henry Chang <mr.changyuheng@gmail.com>
2018-09-29 13:59:02 +0900

    Bump to 1.0.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.1',
+    version='1.0.2',
 
     description='An asynchronous serial port library of Python',
     long_description=long_description,
