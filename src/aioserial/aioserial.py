10466a638d4fec25d9e35b58e919e17a8a12cc5a
Henry Chang <jrhenry@flotechnologies.com>
2020-06-30 22:13:53 +0800

    Ensure `await read*_async()` and `await write*_async()` functions are atomic
    This enhancement is for solving a possible issue caused by reading
    before the cancellation completed. An example code showed below:
    
    ```
    as = AioSerial(...)
    
    r1 = as.read()
    r2 = as.readline()
    asyncio.create_task(asyncio.gather(r1, r2))
    r1.cancel()
    ```

--- src/aioserial/aioserial.py
+++ src/aioserial/aioserial.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Henry Chang
+# Copyright 2020 Henry Chang
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -29,8 +29,9 @@ class AioSerial(serial.Serial):
             dsrdtr: bool = False,
             inter_byte_timeout: Optional[Union[float, int]] = None,
             exclusive: Optional[bool] = None,
-            cancel_timeout: Optional[Union[float, int]] = None,
             loop: Optional[asyncio.AbstractEventLoop] = None,
+            cancel_read_timeout: int = 1,
+            cancel_write_timeout: int = 1,
             **kwargs):
         super().__init__(
             port=port,
@@ -46,17 +47,21 @@ class AioSerial(serial.Serial):
             inter_byte_timeout=inter_byte_timeout,
             exclusive=exclusive,
             **kwargs)
-        self.cancel_timeout: Union[float, int] = \
-            1 if cancel_timeout is None else cancel_timeout
         self._loop: Optional[asyncio.AbstractEventLoop] = loop
-        self._read_executor: concurrent.futures.ThreadPoolExecutor = \
-            concurrent.futures.ThreadPoolExecutor(max_workers=1)
+
         self._cancel_read_executor: concurrent.futures.ThreadPoolExecutor = \
             concurrent.futures.ThreadPoolExecutor(max_workers=1)
-        self._write_executor: concurrent.futures.ThreadPoolExecutor = \
+        self._cancel_read_timeout: int = cancel_read_timeout
+        self._read_executor: concurrent.futures.ThreadPoolExecutor = \
             concurrent.futures.ThreadPoolExecutor(max_workers=1)
+        self._read_lock: asyncio.Lock = asyncio.Lock()
+
         self._cancel_write_executor: concurrent.futures.ThreadPoolExecutor = \
             concurrent.futures.ThreadPoolExecutor(max_workers=1)
+        self._cancel_write_timeout: int = cancel_write_timeout
+        self._write_executor: concurrent.futures.ThreadPoolExecutor = \
+            concurrent.futures.ThreadPoolExecutor(max_workers=1)
+        self._write_lock: asyncio.Lock = asyncio.Lock()
 
     @property
     def loop(self) -> Optional[asyncio.AbstractEventLoop]:
@@ -68,76 +73,85 @@ class AioSerial(serial.Serial):
         self._loop = value
 
     async def _cancel_read_async(self):
-        if hasattr(self, 'cancel_read'):
-            await asyncio.wait_for(
-                self.loop.run_in_executor(
-                    self._cancel_read_executor, self.cancel_read),
-                timeout=self.cancel_timeout)
+        if not hasattr(self, 'cancel_read'):
+            return
+        await asyncio.wait_for(
+            self.loop.run_in_executor(
+                self._cancel_read_executor, self.cancel_read),
+            self._cancel_read_timeout)
+
+    async def _cancel_write_async(self):
+        if not hasattr(self, 'cancel_write'):
+            return
+        await asyncio.wait_for(
+            self.loop.run_in_executor(
+                self._cancel_write_executor, self.cancel_write),
+            self._cancel_write_timeout)
 
     async def read_async(self, size: int = 1) -> bytes:
-        try:
-            return await self.loop.run_in_executor(
-                self._read_executor, self.read, size)
-        except asyncio.CancelledError:
-            await self._cancel_read_async()
-            raise
+        async with self._read_lock:
+            try:
+                return await self.loop.run_in_executor(
+                    self._read_executor, self.read, size)
+            except asyncio.CancelledError:
+                await asyncio.shield(self._cancel_read_async())
+                raise
 
     async def read_until_async(
             self,
             expected: bytes = serial.LF,
             size: Optional[int] = None) -> bytes:
-        try:
-            return await self.loop.run_in_executor(
-                self._read_executor, self.read_until, expected, size)
-        except asyncio.CancelledError:
-            await self._cancel_read_async()
-            raise
+        async with self._read_lock:
+            try:
+                return await self.loop.run_in_executor(
+                    self._read_executor, self.read_until, expected, size)
+            except asyncio.CancelledError:
+                await asyncio.shield(self._cancel_read_async())
+                raise
 
     async def readinto_async(self, b: Union[array.array, bytearray]):
-        try:
-            return await self.loop.run_in_executor(
-                self._read_executor, self.readinto, b)
-        except asyncio.CancelledError:
-            await self._cancel_read_async()
-            raise
+        async with self._read_lock:
+            try:
+                return await self.loop.run_in_executor(
+                    self._read_executor, self.readinto, b)
+            except asyncio.CancelledError:
+                await asyncio.shield(self._cancel_read_async())
+                raise
 
     async def readline_async(self, size: int = -1) -> bytes:
-        try:
-            return await self.loop.run_in_executor(
-                self._read_executor, self.readline, size)
-        except asyncio.CancelledError:
-            await self._cancel_read_async()
-            raise
+        async with self._read_lock:
+            try:
+                return await self.loop.run_in_executor(
+                    self._read_executor, self.readline, size)
+            except asyncio.CancelledError:
+                await asyncio.shield(self._cancel_read_async())
+                raise
 
     async def readlines_async(self, hint: int = -1) -> List[bytes]:
-        try:
-            return await self.loop.run_in_executor(
-                self._read_executor, self.readlines, hint)
-        except asyncio.CancelledError:
-            await self._cancel_read_async()
-            raise
-
-    async def _cancel_write_async(self):
-        if hasattr(self, 'cancel_write'):
-            await asyncio.wait_for(
-                self.loop.run_in_executor(
-                    self._cancel_write_executor, self.cancel_write),
-                timeout=self.cancel_timeout)
+        async with self._read_lock:
+            try:
+                return await self.loop.run_in_executor(
+                    self._read_executor, self.readlines, hint)
+            except asyncio.CancelledError:
+                await asyncio.shield(self._cancel_read_async())
+                raise
 
     async def write_async(
             self, data: Union[bytearray, bytes, memoryview]) -> int:
-        try:
-            return await self.loop.run_in_executor(
-                self._write_executor, self.write, data)
-        except asyncio.CancelledError:
-            await self._cancel_write_async()
-            raise
+        async with self._write_lock:
+            try:
+                return await self.loop.run_in_executor(
+                    self._write_executor, self.write, data)
+            except asyncio.CancelledError:
+                await asyncio.shield(self._cancel_write_async())
+                raise
 
     async def writelines_async(
             self, lines: List[Union[bytearray, bytes, memoryview]]):
-        try:
-            return await self.loop.run_in_executor(
-                self._write_executor, self.writelines, lines)
-        except asyncio.CancelledError:
-            await self._cancel_write_async()
-            raise
+        async with self._write_lock:
+            try:
+                return await self.loop.run_in_executor(
+                    self._write_executor, self.writelines, lines)
+            except asyncio.CancelledError:
+                await asyncio.shield(self._cancel_write_async())
+                raise
